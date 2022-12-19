8f578d8118dc307c81d912c031960e81ed18476c
ElGariehy <algaraiehyemad2901@gmail.com>
2022-12-19 18:38:41 +0200

    edit commit

---
+++ weekOne.py
@@ -0,0 +1,19 @@
+# This
+# is
+# multiple
+# comment
+name = "ElGariehy"
+age = "23"
+country = "Egypt"
+# ------------------------------------
+print("Name: " + name)
+print("Age: " + age)
+print("Country: " + country)
+# ------------------------------------
+# Hello, My Name Is Osama And Iam 38 Years Old and I Live in Egypt.
+print("Hello, My Name Is " + name + " And Iam " +
+      age + " Years Old and I Live in "+country)
+# ------------------------------------
+print(type(name))
+print(type(age))
+print(type(country))
